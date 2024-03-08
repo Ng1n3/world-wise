4cbaf8ad5ffe88621f0bfa94c6bf384a6ce11300
ng1n3 <code.with.muyiwa@gmail.com>
2024-03-08 11:04:30 +0100

    Reading and Setting a Query String

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import styles from "./City.module.css";
 
 const formatDate = (date) =>
@@ -10,7 +10,10 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function City() {
-  const {id} = useParams()
+  const { id } = useParams();
+  const [searchParams, setSetparams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
 
   // TEMP DATA
   const currentCity = {
@@ -22,7 +25,15 @@ function City() {
 
   const { cityName, emoji, date, notes } = currentCity;
 
-  return <h1>City {id}</h1>
+  return (
+    <>
+      <h1>City {id}</h1>
+      <h1>
+        position: {lng}, {lat}
+      </h1>
+
+    </>
+  );
 
   // return (
   //   <div className={styles.city}>
