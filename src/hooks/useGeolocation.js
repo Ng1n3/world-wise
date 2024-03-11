fc9329a5820008c34d2a18f0870d3011072e6a81
ng1n3 <code.with.muyiwa@gmail.com>
2024-03-11 10:33:34 +0100

    create a custom useGeoloaction hook

---
+++ src/hooks/useGeolocation.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export function useGeolocation() {
+  const [isLoading, SetIsLoading] = useState(false);
+  const [position, setPosition] = useState({});
+  const [error, setError] = useState(null);
+
+  function getPosition() {
+    if (!navigator.geolocation)
+      return setError("Your browser does not support geolocation");
+
+    SetIsLoading(true);
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        setPosition({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        });
+        SetIsLoading(false);
+      },
+      (error) => {
+        setError(error.message);
+        SetIsLoading(false);
+      }
+    );
+  }
+
+  return {isLoading, position, error, getPosition}
+}
